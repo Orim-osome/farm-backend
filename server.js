1fa67a17ae4f2c0f1ddea1d99cf8ecda705397e0
Orim-osome <orurutestimony19@gmail.com>
2026-05-02 00:32:47 +0100

    Fix CORS policy to allow Vercel preview origins

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://farmers-app-blond.vercel.app', 
+  origin: '*', 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
